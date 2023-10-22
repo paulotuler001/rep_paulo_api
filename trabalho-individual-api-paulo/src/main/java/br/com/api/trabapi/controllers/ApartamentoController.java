b1183475b868c6810bddf90c3d069708e82e9529
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-22 15:11:44 -0300

    build: package controller + requests

---
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/controllers/ApartamentoController.java
@@ -0,0 +1,54 @@
+package br.com.api.trabapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.g4.entities.Categoria;
+import br.com.api.g4.services.CategoriaService;
+
+@RestController
+@RequestMapping("/apartamento")
+public class ApartamentoController {
+
+	@Autowired
+	ApartamentoService categoriaService;
+
+	@GetMapping("/count")
+	public Integer getCount() {
+		return apartamentoService.getCount();
+	}
+	
+	@PostMapping("/salvar")
+	public Apartamento salvar(@RequestBody Apartamento objetoTeste) {
+		return apartamentoService.salvar(objetoTeste);
+	}
+
+	@GetMapping("/{id}")
+	public Apartamento acharId(@PathVariable Integer id) {
+		return apartamentoService.acharId(id);
+	}
+
+	@GetMapping("/listar")
+	public List<Apartamento> listar() {
+		return apartamentoService.listar();
+	}
+
+	@DeleteMapping("/deletarLogico/{id}")
+	public void deletarlogico(@PathVariable Integer id) {
+		apartamentoService.deletarlogico(id);
+	}
+
+	@PutMapping("/atualizar/{id}")
+	public Apartamento atualizar(@PathVariable Integer id, @RequestBody Apartamento objetoTeste) {
+		return apartamentoService.atualizar(id,objetoTeste);
+	}
+}
