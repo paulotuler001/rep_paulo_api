979ef2bb00764fd9db55ad935f138c2ca9338516
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-22 20:44:10 -0300

    feat: implementação no package config

--- trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/config/SecurityConfig.java
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/config/SecurityConfig.java
@@ -1,18 +1,76 @@
 package br.com.api.trabapi.config;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import br.com.api.trabapi.repositories.UsuarioRepository;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-		http.authorizeHttpRequests().anyRequest().permitAll().and().csrf(csrf -> csrf.disable());
-		return http.build();
-	}
+	@Autowired
+    UsuarioRepository userRepo;
+
+    @Autowired
+    JWTFilter filter;
+
+    @Autowired
+    UserDetailsServiceImpl uds;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    //TODO voltar os paths de segurança certos 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception { // Metodo encarregado de configurar a seguranca da API
+        http
+		        .cors()
+		        .and()
+        		.csrf().disable()
+                .httpBasic().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/categoria/{id}","/categoria/listar","/produto/count","/usuario/salvar","/usuario/login").permitAll()
+                .antMatchers("/usuario/{id}","/usuario/listar","/pedido/salvar").permitAll()//hasRole("COMPRADOR")
+                .antMatchers("/categoria/count","/categoria/salvar","/categoria/deletarLogico/{id}","/categoria/atualizar/{id}","/pedido/atualizar/{id}","/produto/deletar/{id}","/produto/atualizar/{id}","/produto/promocao","/usuario/salvar","/produto/salvar").permitAll()//hasRole("VENDEDOR")
+                .antMatchers("/endereco/count","/endereco/salvar","/endereco/{id}","/endereco/listar","/endereco/deletarLogico/{id}","/endereco/atualizar/{id}","/pedido/count","/pedido/{id}","/pedido/listar","/pedido/deletar/{id}","/produto/{id}","/produto/listar","/usuario/deletarLogico/{id}","/usuario/atualizar/{id}","/usuario/listarEndereco/{id}","/usuario/recuperarSenha/{id}","/usuario/recuperarConta/{id}","/usuario/registro").permitAll()//hasAnyRole("COMPRADOR","VENDEDOR")
+                .and()
+                .userDetailsService(uds)
+                .exceptionHandling()
+                    .authenticationEntryPoint(
+                            (request, response, authException) ->
+                                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+                    )
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+    }
+	
+	
+//	@Bean
+//	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//		http.authorizeHttpRequests().anyRequest().permitAll().and().csrf(csrf -> csrf.disable());
+//		return http.build();
+//	}
 }
---
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/config/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package br.com.api.trabapi.config;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import br.com.api.trabapi.entities.Usuario;
+import br.com.api.trabapi.repositories.RoleRepository;
+import br.com.api.trabapi.repositories.UsuarioRepository;
+import br.com.api.trabapi.services.UsuarioService;
+
+	@Component
+	public class UserDetailsServiceImpl implements UserDetailsService {
+
+	    @Autowired 
+	    UsuarioRepository userRepo;
+
+	    @Autowired
+	    UsuarioService userService;
+
+	    @Autowired 
+	    RoleRepository roleRepo;
+
+		@Override
+	    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	        Optional<Usuario> userRes = userRepo.findByEmail(email);
+	        if(userRes.isEmpty()) {
+	            throw new UsernameNotFoundException("Não foi possível encontrar usuário com o email = " + email);
+	        }
+		return new org.springframework.security.core.userdetails.User(
+	                email,
+	                userRes.get().getPassword(),
+	                roleRepo.roles(email).stream()
+					.map(role -> new SimpleGrantedAuthority(role.getName()
+							.name())).collect(Collectors.toList())); // Define, de forma estatica, o perfil do usuario encontrado
+	    }
+}
