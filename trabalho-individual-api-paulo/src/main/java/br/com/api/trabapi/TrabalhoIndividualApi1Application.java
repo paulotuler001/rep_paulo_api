1f0684b3ca441e991f67454a440595e5b7f169ae
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-22 14:04:24 -0300

    feat: commit inicial

---
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/TrabalhoIndividualApi1Application.java
@@ -0,0 +1,13 @@
+package br.com.api.trabapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoIndividualApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoIndividualApi1Application.class, args);
+	}
+
+}
