a5657fa207ea71e8092acd321e9ddefea30c8bc2
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-22 16:09:48 -0300

    build: +repository

---
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/repositories/ApartamentoRepository.java
@@ -0,0 +1,15 @@
+package br.com.api.trabapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.trabapi.entities.Apartamento;
+
+@Repository
+public interface ApartamentoRepository extends JpaRepository<Apartamento, Integer> {
+
+	@Query(value = "select count(*) from categoria ", nativeQuery = true)
+	public Integer contar();
+
+}
---
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/repositories/FiadorRepository.java
@@ -0,0 +1,15 @@
+package br.com.api.trabapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import br.com.api.trabapi.entities.Fiador;
+
+@Repository
+public interface FiadorRepository extends JpaRepository<Fiador, Integer> {
+
+	@Query(value = "select count(*) from categoria ", nativeQuery = true)
+	public Integer contar();
+
+}
