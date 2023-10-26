a27d9130cbfc80a552140aa74f1f5a9d7fa112fb
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-26 16:40:08 -0300

    fix: tentei tudo o que eu pude

---
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/dto/UsuarioRespostaDTO.java
@@ -0,0 +1,58 @@
+package br.com.api.trabapi.dto;
+
+import java.util.List;
+
+import br.com.api.trabapi.entities.Endereco;
+
+public class UsuarioRespostaDTO {
+
+	public String nome;
+	public String nomeUsuario;
+	public String email;
+	private List<Endereco> endereco;
+
+	public UsuarioRespostaDTO() {
+		super();
+	}
+
+	public UsuarioRespostaDTO(String nome, String nomeUsuario, String email, List<Endereco> endereco) {
+		super();
+		this.nome = nome;
+		this.nomeUsuario = nomeUsuario;
+		this.email = email;
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Endereco> getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(List<Endereco> endereco) {
+		this.endereco = endereco;
+	}
+
+}
