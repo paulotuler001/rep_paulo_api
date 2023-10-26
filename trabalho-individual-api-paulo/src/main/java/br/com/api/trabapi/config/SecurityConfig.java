98abc31b61134705105d2ed72fa568aae99ca72e
paulotulerfaeterj <paulogustavotuler@gmail.com>
2023-10-26 16:38:53 -0300

    fix: botando permissoes

--- trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/config/SecurityConfig.java
+++ trabalho-individual-api-paulo/src/main/java/br/com/api/trabapi/config/SecurityConfig.java
@@ -21,53 +21,49 @@ import br.com.api.trabapi.repositories.UsuarioRepository;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-    UsuarioRepository userRepo;
+	UsuarioRepository userRepo;
 
-    @Autowired
-    JWTFilter filter;
+	@Autowired
+	JWTFilter filter;
 
-    @Autowired
-    UserDetailsServiceImpl uds;
+	@Autowired
+	UserDetailsServiceImpl uds;
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
-    //TODO voltar os paths de segurança certos 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { // Metodo encarregado de configurar a seguranca da API
-        http
-		        .cors()
-		        .and()
-        		.csrf().disable()
-                .httpBasic().disable()
-                .authorizeHttpRequests()
-                .antMatchers("/apartamento/{id}","/apartamento/listar","/apartamento/count","/usuario/salvar","/usuario/login").permitAll()
-                .antMatchers("/usuario/{id}","/usuario/listar","/apartamento/salvar").permitAll()//hasRole("COMPRADOR")
-                .antMatchers("/fiador/count","/apartamento/salvar","/apartamento/deletarLogico/{id}","/apartamento/atualizar/{id}","/inquilino/atualizar/{id}","/fiador/deletar/{id}","/fiador/atualizar/{id}","/usuario/salvar","/fiador/salvar").permitAll()//hasRole("VENDEDOR")
-                .antMatchers("/endereco/count","/endereco/salvar","/endereco/{id}","/endereco/listar","/endereco/deletarLogico/{id}","/endereco/atualizar/{id}","/inquilino/count","/inquilino/{id}","/inquilino/listar","/inquilino/deletar/{id}","/fiador/{id}","/fiador/listar","/usuario/deletarLogico/{id}","/usuario/atualizar/{id}","/usuario/listarEndereco/{id}","/usuario/recuperarSenha/{id}","/usuario/recuperarConta/{id}","/usuario/registro").permitAll()//hasAnyRole("COMPRADOR","VENDEDOR")
-                .and()
-                .userDetailsService(uds)
-                .exceptionHandling()
-                    .authenticationEntryPoint(
-                            (request, response, authException) ->
-                                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
-                    )
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
-    }
-	
-	
+	// TODO voltar os paths de segurança certos
+	@Override
+	protected void configure(HttpSecurity http) throws Exception { // Metodo encarregado de configurar a seguranca da
+																	// API
+		http.cors().and().csrf().disable().httpBasic().disable().authorizeHttpRequests()
+				.antMatchers("/apartamento/{id}", "/apartamento/listar", "/apartamento/count", "/usuario/salvar",
+						"/usuario/login")
+				.permitAll().antMatchers("/usuario/{id}", "/usuario/listar", "/apartamento/salvar").permitAll()// hasRole("COMPRADOR")
+				.antMatchers("/apartamento/salvar", "/apartamento/deletarLogico/{id}", "/apartamento/atualizar/{id}",
+						"/usuario/salvar")
+				.hasRole("PROPRIETARIO")// hasRole("VENDEDOR")
+				.antMatchers("/endereco/count", "/endereco/salvar", "/endereco/{id}", "/endereco/listar",
+						"/endereco/deletarLogico/{id}", "/endereco/atualizar/{id}", "/usuario/deletarLogico/{id}",
+						"/usuario/atualizar/{id}", "/usuario/listarEndereco/{id}", "/usuario/recuperarSenha/{id}",
+						"/usuario/recuperarConta/{id}", "/usuario/registro")
+				.hasRole("PROPRIETARIO")// hasAnyRole("COMPRADOR","VENDEDOR")
+				.and().userDetailsService(uds).exceptionHandling()
+				.authenticationEntryPoint((request, response, authException) -> response
+						.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"))
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+	}
+
 //	@Bean
 //	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 //		http.authorizeHttpRequests().anyRequest().permitAll().and().csrf(csrf -> csrf.disable());
